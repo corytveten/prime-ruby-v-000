4bf72a013a93e01fff74525396f0a37b036e770c
corytveten <corytveten@gmail.com>
2018-05-03 15:44:44 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,6 @@
 # Add  code here!
 def prime?(integer)
-  if integer < 2
-    false
-  elsif integer % 2 == 0
-    false
-  elsif integer % 3 == 0
-    false
-  elsif integer % 4 == 0
-    false
-  elsif integer % 5 == 0
-    false
-  else
-    true
-  end
+  return false if (number.even? && number > 2) || number <= 1
+  
 
 end
