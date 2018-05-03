442b73d7ca1d769869c65e4fc344eb07893a1297
corytveten <corytveten@gmail.com>
2018-05-03 15:38:14 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,9 @@
 def prime?(integer)
   if integer % 2 == 0
     false
-  elsif integer & 3 == 0
+  elsif integer % 3 == 0
     false
-  elsif integer & 4 == 0
+  elsif integer % 4 == 0
     false
   elsif integer % 5 == 0
     false
