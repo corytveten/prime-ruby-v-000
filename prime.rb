824406d70772bbfa85e829c1a70e0f1dc15fa4ae
corytveten <corytveten@gmail.com>
2018-05-03 15:37:24 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(integer)
+  if integer % 2 == 0
+    false
+  elsif integer & 3 == 0
+    false
+  elsif integer & 4 == 0
+    false
+  elsif integer % 5 == 0
+    false
+  else
+    true
+  end
+
+end
